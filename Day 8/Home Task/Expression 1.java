4da6f120b9d860cd7460a2a3467a22cd7d53d208
DHANALINGAM L <dhanalingaml@DHANALINGAMs-MacBook-Air.local>
2026-07-14 09:44:11 +0530

    Initial commit

--- Day 8/Home Task/Expression 1.java
+++
@@ -1,9 +0,0 @@
-class Expression1 {
-    public static void main(String[] args) {
-        int a = 4;
-        int b = 3;
-        System.out.println(++a * b-- + a-- - ++b);
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
